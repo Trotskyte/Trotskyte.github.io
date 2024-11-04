dff144acfbc6aa3430decf61beb23a91199a1dbe
Trotskyte <70798754+Trotskyte@users.noreply.github.com>
2024-11-04 10:38:31 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+el: "#Vanta",
+mouseControls: true,
+touchControls: true,
+gyroControls: false,
+minHeight: 200.00,
+minWidth: 200.00,
+scale: 1.00,
+scaleMobile: 1.00,
+backgroundAlpha: 0.0,
+quantity: 3.00,
+speedlimit: 3.00,
+birdSize: 2.00,
+})
+})
